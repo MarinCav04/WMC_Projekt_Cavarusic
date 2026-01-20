4cadd348f614f3aa3aaf1fdcf86fd965057c8b18
MarinCav04 <marin.cavarusi@gmail.com>
2026-01-20 13:36:53 +0100

    Update validation.js

--- JS/validation.js
+++ JS/validation.js
@@ -1,4 +1,22 @@
 const meinformular = document.getElementById('registration');
 meinformular.onsubmit = function(event) {
-    alert("Super, dein Team wurde erfolgreich angemeldet")
+    // verhinder neues laden der Seite
+    event.preventDefault();
+
+    let team = document.getElementById('teamname').value;
+    let email = document.getElementById('email').value;
+
+    // prüft ob teamname leer steht
+    if (team == "") {
+        alert("Bitte Teamnamen eingeben!")
+        return;
+    }
+    // prüft ob ein @ in der mail steht
+    if (email.includes("@") == false) {
+        alert("Geben Sie eine richtige E-Mail Adresse ein!")
+        return;
+    }
+    alert("Dein Team wurde erfolgreich angemeldet");
+    // löschen der Daten beim absenden
+    meinformular.reset();
 };
